0c1d3d1e029996d8657aac332adc6cccb34a5a43
Qgel <kug3lblitz@gmail.com>
2016-03-21 23:10:03 +0100

    Added inline drink icons

--- cards/deck.rb
+++ cards/deck.rb
@@ -2,7 +2,7 @@ require 'squib'
 
 colors = {
   'status' => '#63b0ef',
-  'action' => '#ff0000',
+  'action' => '#f46400',
 }
 
 data = Squib.csv file: 'data/action.csv'
@@ -10,17 +10,21 @@ Squib::Deck.new(cards: data['name'].size, layout: 'action.yml') do
   png file: 'img/background.png', layout: :Background
   svg file: 'img/target.svg', layout: :Target_Icon
 
-  svg file: data['art'].map{ |art| 'img/art/' + art + '.svg' }, layout: :Art
+  svg file: data['art'].map{ |art| "img/art/#{art}.svg" }, layout: :Art
 
   text str: data['name'], color: data['type'].map{|t| colors[t]}, layout: :Name
 
-  text str: data['description'], layout: :Description 
+  text(str: data['description'], layout: :Description) do |embed|
+    %w(shot beer).each do |drink|
+      embed.svg key: ":#{drink}:", file: "img/#{drink}.svg", width: 32, dx: 3, height: :scale
+    end
+  end
 
-  text str: data['flavor'].map{|f| '~ ' + f + ' ~'}, layout: :Flavor
+  text str: data['flavor'].map{|f| "~ #{f} ~"}, layout: :Flavor
 
   text str: data['target'], layout: :Target
 
-  svg file: data['type'].map{|t| 'img/type_'+t+'.svg'}, layout: :Type
+  svg file: data['type'].map{|t| "img/type_#{t}.svg"}, layout: :Type
 
   for n in 1..3
     range = data['cost'].each_index.select{ |i| data['cost'][i] >= n and data['cost'][i] <= 3}
